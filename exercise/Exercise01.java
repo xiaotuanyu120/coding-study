f21b9869b4887b2611039d4d825b37a3edceabf5
xiaotuanyu120 <zhaopeiwu@outlook.com>
2020-06-26 16:00:39 +0800

    20200626

--- exercise/Exercise01.java
+++ exercise/Exercise01.java
@@ -1,4 +1,6 @@
 import java.util.Arrays;
+import java.util.Random;
+import java.lang.Math;
 
 
 public class Exercise01 {
@@ -29,6 +31,15 @@ public class Exercise01 {
         exercise118();
         exercise119();
         exercise1110();
+        exercise1111();
+        exercise1112();
+        exercise1113();
+        exercise1114();
+        exercise1115();
+        System.out.println("1.1.16");
+        System.out.println(exercise1116(6));
+        System.out.println();
+        exercise1117();
 	}
 
 	public static void exercise111() {
@@ -165,6 +176,171 @@ public class Exercise01 {
             a[i] = i*i;
         }
         System.out.println(Arrays.toString(a));
+        System.out.println();
+    }
+
+    public static void exercise1111() {
+        System.out.println("1.1.11");
+        int length = 5;
+        int width = 6;
+        String[][] a = new String[length][width];
+        Random rd = new Random();
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < width; j++) {
+                a[i][j] = rd.nextBoolean() ? "*" : " ";
+            }
+        }
+        String width_title = " ";
+        for (int j = 0; j < width; j++) {
+            width_title += " ";
+            width_title += Integer.toString(j+1);
+        }
+        System.out.println(width_title);
+        for (int i = 0; i < length; i++) {
+            String row = Integer.toString(i+1);
+            for (int j = 0; j < width; j++) {
+                row += " ";
+                row += a[i][j];
+            }
+            System.out.println(row);
+        }
+        System.out.println();
+    }
+
+    public static void exercise1112() {
+        System.out.println("1.1.12");
+        int[] a = new int[10];
+        for (int i = 0; i < 10; i++) {
+           a[i] = 9 - i;
+        }
+        for (int i = 0; i < 10; i++) {
+           a[i] = a[a[i]];
+        }
+        for (int i = 0; i < 10; i++) {
+           System.out.println(i);
+        }
+        System.out.println();
+    }
+
+    public static void exercise1113() {
+        System.out.println("1.1.13");
+        int length = 5;
+        int width = 6;
+        int[][] a = new int[length][width];
+        Random rd = new Random();
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < width; j++) {
+                a[i][j] = rd.nextInt(10);
+            }
+        }
+        System.out.println("The raw arrays");
+        for (int i = 0; i < length; i++) {
+            String row = "";
+            for (int j = 0; j < width; j++) {
+                row += " " + Integer.toString(a[i][j]);
+            }
+            System.out.println(row);
+        }
+        System.out.println("The rotated arrays");
+        for (int j = 0; j < width; j++) {
+            String row = "";
+            for (int i = 0; i < length; i++) {
+                row += " " + Integer.toString(a[i][j]);
+            }
+            System.out.println(row);
+        }
+        System.out.println();
+    }
+
+    public static void exercise1114() {
+        System.out.println("1.1.14");
+        int result = 0;
+        long n1 = 1;
+        long n2 = 2;
+
+        Random rd = new Random();
+        int n = rd.nextInt();
+        if (n < 0) {
+            n *= -1;
+        }
+        System.out.printf("Generated number: %d\n", n);
+
+        // debug
+        System.out.printf("Math moudle debug result: %f\n", Math.log(n)/Math.log(2));
+
+        for (int i = 0; i < 31; i++) {
+            if ( n == 1 ) {
+                break;
+            } else if ( n > n1 && n <= n2 ) {
+                break;
+            } else if ( n < n1 ) {
+                System.out.println("Something must be wrong!");
+                break;
+            } else {
+                n1 = n2;
+                n2 *= 2;
+                result += 1;
+            }
+        }
+        System.out.printf("Result1: %d\n", result);
+
+        result = 0;
+        for (int i =0; i < 31; i++) {
+            if ( n == 1 ) {
+                break;
+            }
+            n = n >> 1;
+            result += 1;
+        }
+        System.out.printf("Result2: %d\n", result);
+        System.out.println();
+    }
+
+    public static void exercise1115() {
+        System.out.println("1.1.15");
+        Random rd = new Random();
+        int m = rd.nextInt(100);
+        int[] a = new int[rd.nextInt(100)];
+        System.out.printf("array a: ");
+        for (int i = 0; i < a.length; i++) {
+            a[i] = rd.nextInt(m);
+            System.out.printf("%d,", a[i]);
+        }
+        System.out.println();
+
+        int[] result = new int[m];
+        int sum = 0;
+        System.out.printf("array result: ");
+        for (int i = 0; i < m; i++) {
+            int count = 0;
+            for (int j = 0; j < a.length; j++) {
+                if ( a[j] == i ) {
+                    count += 1;
+                }
+            }
+            result[i] = count;
+            System.out.printf("%d,", result[i]);
+            sum += result[i];
+        };
+        System.out.println();
+
+        if ( sum == a.length ) {
+            System.out.println("equal");
+        } else {
+            System.out.println("not equal");
+        }
+
+        System.out.println();
+    }
+
+    public static String exercise1116(int n) {
+        if (n <= 0) return "";
+        return exercise1116(n-3) + n + exercise1116(n-2) + n;
+    }
+
+    public static void exercise1117() {
+        System.out.println("1.1.17");
+        System.out.println("跳出递归条件，必须要在递归函数内部调用之前");
         System.out.println();
     }
 }
